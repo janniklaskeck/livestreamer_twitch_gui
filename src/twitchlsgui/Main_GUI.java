aff3bc58dfe862f70c720667520916f82a7733b1
westerwave <neowow@online.de>
2015-02-01 04:59:49 +0100

    forgot adjusting the version number

--- src/twitchlsgui/Main_GUI.java
+++ src/twitchlsgui/Main_GUI.java
@@ -64,7 +64,7 @@ import twitchUpdate.StreamCheck;
  */
 public class Main_GUI extends JFrame {
 
-    public static final Version VERSION = new Version(1, 6, 0, 0);
+    public static final Version VERSION = new Version(1, 7, 0, 0);
     public static boolean _DEBUG = false;
 
     private static final long serialVersionUID = 1L;
