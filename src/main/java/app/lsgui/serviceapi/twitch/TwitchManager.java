1ad5d645c3d622b6de7797c9e267b5430f80dbbf
westerwave <neowow@online.de>
2016-03-24 19:19:52 +0100

    some empty methods

--- src/main/java/app/lsgui/serviceapi/twitch/TwitchManager.java
+++ src/main/java/app/lsgui/serviceapi/twitch/TwitchManager.java
@@ -1,5 +1,40 @@
 package app.lsgui.serviceapi.twitch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class TwitchManager {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TwitchManager.class);
+	private static TwitchManager instance = null;
+	private static final JsonParser JSONPARSER = new JsonParser();
+
+	private TwitchManager() {
+		LOGGER.debug("TwitchManager constructed");
+	}
+
+	public static TwitchManager instance() {
+		if (instance == null) {
+			instance = new TwitchManager();
+		}
+		return instance;
+	}
+
+	public JsonObject getStreamData(final String streamName) {
+		// TODO
+		return null;
+	}
+
+	public JsonObject getGameData(final String gameName) {
+		// TODO
+		return null;
+	}
+
+	public JsonObject getChannelData(final String channelName) {
+		// TODO
+		return null;
+	}
 }
