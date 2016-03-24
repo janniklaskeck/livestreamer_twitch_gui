3b02f1be9cb447f15ee8f869e1a48b9b5190e413
westerwave <neowow@online.de>
2016-03-24 19:00:11 +0100

    set loggin manager

--- src/main/java/app/lsgui/Main.java
+++ src/main/java/app/lsgui/Main.java
@@ -1,14 +1,22 @@
 package app.lsgui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import app.lsgui.gui.MainWindow;
 import javafx.application.Application;
 
 public class Main {
 
-    public static void main(String[] args) {
+	private final static Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-        Application.launch(MainWindow.class, args);
+	public static void main(String[] args) {
+		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
 
-    }
+		LOGGER.debug("PRE-Launch finished");
+
+		Application.launch(MainWindow.class, args);
+
+	}
 
 }
