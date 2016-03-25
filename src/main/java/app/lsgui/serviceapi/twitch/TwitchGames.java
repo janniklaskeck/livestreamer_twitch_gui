fca092b9f7ac7d231fa4cd1d50a62bf6d0701017
westerwave <neowow@online.de>
2016-03-25 21:07:45 +0100

    start implementation

--- src/main/java/app/lsgui/serviceapi/twitch/TwitchGames.java
+++ src/main/java/app/lsgui/serviceapi/twitch/TwitchGames.java
@@ -1,18 +1,33 @@
 package app.lsgui.serviceapi.twitch;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.google.gson.JsonObject;
-import com.sun.javafx.collections.MappingChange.Map;
 
 import app.lsgui.model.twitch.TwitchGameModel;
 
 public class TwitchGames {
 
-	private Map<String, TwitchGameModel> gamesMap;
+    private Map<String, TwitchGameModel> gamesMap;
 
-	public TwitchGames(JsonObject twitchData) {
-		gamesMap = new Map<String, TwitchGameModel>();
-	}
+    public TwitchGames(JsonObject twitchData) {
+        gamesMap = new HashMap<String, TwitchGameModel>();
+    }
+
+    /**
+     * @return the gamesMap
+     */
+    public Map<String, TwitchGameModel> getGamesMap() {
+        return gamesMap;
+    }
+
+    /**
+     * @param gamesMap
+     *            the gamesMap to set
+     */
+    public void setGamesMap(Map<String, TwitchGameModel> gamesMap) {
+        this.gamesMap = gamesMap;
+    }
 
 }
