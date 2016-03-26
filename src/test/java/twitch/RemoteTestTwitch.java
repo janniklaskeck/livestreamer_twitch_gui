ab16ccfe83776b905273dbe9e380cb802958b797
westerwave <neowow@online.de>
2016-03-27 00:53:28 +0100

    change test to followedstreams

--- src/test/java/twitch/RemoteTestTwitch.java
+++ src/test/java/twitch/RemoteTestTwitch.java
@@ -6,14 +6,11 @@ import app.lsgui.serviceapi.twitch.TwitchProcessor;
 
 public class RemoteTestTwitch {
 
-    TwitchProcessor tm = TwitchProcessor.instance();
+    TwitchProcessor tp = TwitchProcessor.instance();
 
     @Test
-    public void testGetAPIResponse() {
-        @SuppressWarnings("deprecation")
-        String a = tm.readJsonFromUrl("https://api.twitch.tv/kraken/streams/elajjaz");
-        String b = tm.getAPIResponse("https://api.twitch.tv/kraken/streams/elajjaz");
-        assert (a.equals(b));
+    public void test() {
+        System.out.println(tp.getListOfFollowedStreams("nuamor"));
     }
 
 }
