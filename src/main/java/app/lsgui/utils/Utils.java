a19f5ae66e3e500d1f850b03b4a470084bad91a0
westerwave <neowow@online.de>
2016-03-26 01:05:23 +0100

    add some empty static methods

--- src/main/java/app/lsgui/utils/Utils.java
+++ src/main/java/app/lsgui/utils/Utils.java
@@ -2,4 +2,16 @@ package app.lsgui.utils;
 
 public class Utils {
 
+    public static void startLivestreamer(String URL, String quality) {
+
+    }
+
+    public static void recordLivestreamer(String URL, String quality) {
+
+    }
+
+    public static void openURLInBrowser(String URL) {
+
+    }
+
 }
