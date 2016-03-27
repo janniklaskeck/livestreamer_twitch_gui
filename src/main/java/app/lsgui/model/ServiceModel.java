fb1b51c88883e138ab926f2c8d20ebd04547da68
westerwave <neowow@online.de>
2016-03-28 01:56:29 +0200

    basic functions

--- src/main/java/app/lsgui/model/ServiceModel.java
+++ src/main/java/app/lsgui/model/ServiceModel.java
@@ -1,5 +1,26 @@
 package app.lsgui.model;
 
+import java.util.List;
+
 public class ServiceModel {
+
+    public ServiceModel(String asString, String asString2) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public List<StreamModel> getChannels() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getUrl() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
