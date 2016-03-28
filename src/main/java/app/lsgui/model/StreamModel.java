8aea59e432ecab902b5581a8ea80f81c7eb82766
westerwave <neowow@online.de>
2016-03-28 17:46:04 +0200

    add online getter

--- src/main/java/app/lsgui/model/StreamModel.java
+++ src/main/java/app/lsgui/model/StreamModel.java
@@ -1,9 +1,11 @@
 package app.lsgui.model;
 
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.StringProperty;
 
 public interface StreamModel {
 
     public StringProperty getName();
+    public BooleanProperty getOnline();
 
 }
