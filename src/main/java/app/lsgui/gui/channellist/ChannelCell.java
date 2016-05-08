d536e31774bdf58bc26896ec1f71c223099ac0cd
westerwave <neowow@online.de>
2016-05-08 21:20:22 +0200

    add custom css pseudo class

--- src/main/java/app/lsgui/gui/channellist/ChannelCell.java
+++ src/main/java/app/lsgui/gui/channellist/ChannelCell.java
@@ -4,13 +4,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import app.lsgui.model.Channel;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.BooleanPropertyBase;
+import javafx.css.PseudoClass;
 import javafx.scene.control.ListCell;
-import javafx.scene.paint.Color;
 
 public class ChannelCell extends ListCell<Channel> {// NOSONAR
 
     @SuppressWarnings("unused")
     private static final Logger LOGGER = LoggerFactory.getLogger(ChannelCell.class); // NOSONAR
+    private static final PseudoClass ONLINEPSEUDOCLASS = PseudoClass.getPseudoClass("online");
+
+    private BooleanProperty online;
+
+    public ChannelCell() {
+        online = new BooleanPropertyBase() {
+            @Override
+            public void invalidated() {
+                pseudoClassStateChanged(ONLINEPSEUDOCLASS, get());
+            }
+
+            @Override
+            public String getName() {
+                return "online";
+            }
+
+            @Override
+            public Object getBean() {
+                return ChannelCell.this;
+            }
+        };
+        getStyleClass().add("channel-cell");
+    }
 
     @Override
     protected void updateItem(Channel item, boolean isEmpty) {
@@ -19,13 +44,15 @@ public class ChannelCell extends ListCell<Channel> {// NOSONAR
             setText(null);
         } else {
             setText(item.getName().get());
-            if (item.isOnline().get()) {
-                setStyle("-fx-background-color: green");
-                setTextFill(Color.BLACK);
-            } else {
-                setStyle("");
-                setTextFill(Color.BLACK);
-            }
+            online.bind(item.isOnline());
         }
+    }
+
+    public boolean getIsOnline() {
+        return online.get();
+    }
+
+    public void setIsOnline(boolean isOnline) {
+        this.online.setValue(isOnline);
     }
 }
