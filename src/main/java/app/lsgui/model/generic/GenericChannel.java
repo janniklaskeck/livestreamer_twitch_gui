ef864f7a3c429853d99c4ada450ec7ef04c3b1fc
westerwave <neowow@online.de>
2016-05-12 19:23:17 +0200

    add generic channel class

---
+++ src/main/java/app/lsgui/model/generic/GenericChannel.java
@@ -0,0 +1,44 @@
+package app.lsgui.model.generic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import app.lsgui.model.Channel;
+import javafx.beans.Observable;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.util.Callback;
+
+public class GenericChannel implements Channel {
+
+    private StringProperty nameProperty;
+    private BooleanProperty onlineProperty;
+
+    public GenericChannel(final String name) {
+        this.nameProperty = new SimpleStringProperty();
+        this.onlineProperty = new SimpleBooleanProperty();
+        this.nameProperty.setValue(name);
+        this.onlineProperty.setValue(true);
+    }
+
+    @Override
+    public StringProperty getName() {
+        return nameProperty;
+    }
+
+    @Override
+    public BooleanProperty isOnline() {
+        return onlineProperty;
+    }
+
+    @Override
+    public List<String> getAvailableQualities() {
+        return Arrays.asList("Worst", "Best");
+    }
+
+    public static Callback<Channel, Observable[]> extractor() {
+        return (Channel sm) -> new Observable[] { ((GenericChannel) sm).getName(), ((GenericChannel) sm).isOnline() };
+    }
+}
