1245b486672270117a6602699c7ecd19a3e88ef5
westerwave <neowow@online.de>
2016-09-25 15:30:53 +0200

    add logging

--- src/main/java/app/lsgui/gui/main/QualityComboBox.java
+++ src/main/java/app/lsgui/gui/main/QualityComboBox.java
@@ -1,10 +1,13 @@
 package app.lsgui.gui.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import app.lsgui.utils.Settings;
 import javafx.scene.control.ComboBox;
 
 public final class QualityComboBox extends ComboBox<String> {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(QualityComboBox.class);
     private static final String OFFLINEQUALITY = "Channel is offline";
 
     public QualityComboBox() {
@@ -14,6 +17,7 @@ public final class QualityComboBox extends ComboBox<String> {
     public void initialize() {
         getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
             if (!OFFLINEQUALITY.equals(newValue)) {
+                LOGGER.debug("Set selected Quality to {}", newValue);
                 Settings.getInstance().setQuality(newValue);
             }
         });
